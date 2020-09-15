e14a5c212d9b452d03bccef8d2d3984022c68d8f
zuowen.Wang <798389920@qq.com>
2020-09-15 15:48:47 +0800

    refactor js

--- src/app.js
+++ src/app.js
@@ -1,41 +1,28 @@
 import "./style/index.scss";
+import {
+  getUrl,
+  getEducationsUrl,
+  getImgElement,
+  getAboutMeElement,
+  getUlElement,
+} from "./util";
 
 $(window).load(function () {
   const localUrl = window.location.href;
-  const url = localUrl.replace("1234", "3000");
-  const educationsUrl = `${url}/educations`;
+  const url = getUrl(localUrl);
+  const educationsUrl = getEducationsUrl(url);
   $.get(url, function (data) {
     console.log(data);
     const nameAndAge = `${data.name} ${data.age}`;
-    const img = document.createElement("img");
-    const aboutMe = document.createElement("p");
-    aboutMe.setAttribute("id", "aboutMe");
-    img.setAttribute("class", "img");
-    img.setAttribute("src", data.avatar);
+    const img = getImgElement(data);
+    const aboutMe = getAboutMeElement(data);
     $(".name_and_age").text(nameAndAge);
     $(".title").before(img);
     $(".about_me b").after(aboutMe);
-    $("#aboutMe").text(data.description);
   });
   $.get(educationsUrl, function (data) {
     console.log(data);
-    const ul = document.createElement("ul");
-    ul.setAttribute("class", "descriptions");
-    data.forEach((education) => {
-      const li = document.createElement("li");
-      const year = document.createElement("span");
-      year.innerHTML = education.year;
-      const p = document.createElement("p");
-      const b = document.createElement("b");
-      const span = document.createElement("span");
-      b.innerHTML = education.title;
-      span.innerHTML = education.description;
-      p.append(b);
-      p.append(span);
-      li.append(year);
-      li.append(p);
-      ul.append(li);
-    });
+    const ul = getUlElement(data);
     $(".education").append(ul);
   });
 });
---
+++ src/util.js
@@ -0,0 +1,41 @@
+export function getUrl(localUrl) {
+  return localUrl.replace("1234", "3000");
+}
+
+export function getEducationsUrl(url) {
+  return `${url}/educations`;
+}
+
+export function getImgElement(data) {
+  const img = document.createElement("img");
+  img.setAttribute("class", "img");
+  img.setAttribute("src", data.avatar);
+  return img;
+}
+
+export function getAboutMeElement(data) {
+  const aboutMe = document.createElement("p");
+  aboutMe.innerHTML = data.description;
+  return aboutMe;
+}
+
+export function getUlElement(data) {
+  const ul = document.createElement("ul");
+  ul.setAttribute("class", "descriptions");
+  data.forEach((education) => {
+    const li = document.createElement("li");
+    const year = document.createElement("span");
+    year.innerHTML = education.year;
+    const p = document.createElement("p");
+    const b = document.createElement("b");
+    const span = document.createElement("span");
+    b.innerHTML = education.title;
+    span.innerHTML = education.description;
+    p.append(b);
+    p.append(span);
+    li.append(year);
+    li.append(p);
+    ul.append(li);
+  });
+  return ul;
+}
